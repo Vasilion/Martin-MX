04b88e7ef5f717c5153e85fa2043d2c037ea2c79
marcusshepp <marcusshepdotcom@gmail.com>
2024-11-09 08:43:23 -0500

    header and hero progress

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MobileService } from '../services/mobile.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    standalone: true,
+    imports: [CommonModule],
+    selector: 'app-header',
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.css'],
+})
+export class HeaderComponent {
+    constructor(private mobileService: MobileService) {}
+
+    public get isMobile$(): Observable<boolean> {
+        return this.mobileService.isHandset();
+    }
+}
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-hero',
+    templateUrl: 'hero.component.html',
+    styleUrls: ['hero.component.css'],
+})
+export class HeroComponent {
+    constructor() {}
+}
