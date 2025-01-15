d7e133447c5ebc81a51698933e7c9c6103c8a057
marcusshepp <marcusshepdotcom@gmail.com>
2025-01-14 21:17:43 -0500

    prod stripe links

--- src/app/membership-pricing/membership-pricing.component.ts
+++ src/app/membership-pricing/membership-pricing.component.ts
@@ -3,40 +3,39 @@ import { ApiService } from "../services/api.service";
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
-
 @Component({
-    standalone: true,
-    imports: [CommonModule, RouterModule],
-    selector: 'app-events',
-    templateUrl: 'membership-pricing.component.html',
-    styleUrls: ['membership-pricing.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  selector: "app-events",
+  templateUrl: "membership-pricing.component.html",
+  styleUrls: ["membership-pricing.component.css"],
 })
 export class MembershipPricingComponent {
-    /*
-     * YOU TALKING ABOUT PRACTICE??
-     */
-    public openPractice: any;
-    public yearlySignUp: any;
-    public yearlyMembershipStripeLink = 'https://buy.stripe.com/test_9AQ00hcTV8we06I9AA';
-    public openPracticeStripeLink = 'https://buy.stripe.com/test_00g8wN3jl13M5r2dQR';
+  /*
+   * YOU TALKING ABOUT PRACTICE??
+   */
+  public openPractice: any;
+  public yearlySignUp: any;
+  public yearlyMembershipStripeLink =
+    "https://buy.stripe.com/7sI0081086Xr4so4gh";
+  public openPracticeStripeLink = "https://buy.stripe.com/6oEcMUgZ6chLe2Y288";
 
-  constructor(private  apiServivce: ApiService) {
-      this.getPricing();
+  constructor(private apiServivce: ApiService) {
+    this.getPricing();
   }
 
   private getPricing() {
     this.apiServivce.getOpenSignUp().subscribe((response: any) => {
-        if (!response) {
-            return;
-        }
-        this.openPractice = response.data.attributes;
+      if (!response) {
+        return;
+      }
+      this.openPractice = response.data.attributes;
     });
     this.apiServivce.getUnlimitedSignUp().subscribe((response: any) => {
-        if (!response) {
-            return;
-        }
-        this.yearlySignUp = response.data.attributes;
+      if (!response) {
+        return;
+      }
+      this.yearlySignUp = response.data.attributes;
     });
   }
-
 }
